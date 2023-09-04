7de76208585279841bb9ba22fadd4e8f5d13cc69
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-09-04 11:07:41 +0700

    Initial Commit

---
+++ Foods/FoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodsApp.swift
+//  Foods
+//
+//  Created by Putut Yusri Bahtiar on 04/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
